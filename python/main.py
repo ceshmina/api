29b10a51576f3414a9b7fd9efd05558557e905b4
ceshmina <shu@ceshmina.com>
2023-03-02 00:04:12 +0900

    Dockerfileの配置を見直し、開発用コンテナをデバッグモードで起動

--- python/main.py
+++ python/main.py
@@ -10,6 +10,15 @@ def index():
     return 'Hello world!'
 
 
+@app.route('/diary')
+def diary():
+    return 'まだ日記はありません'
+
+
 if __name__ == '__main__':
-    port = int(os.environ['PORT'])
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 8080))
+    flask_env = os.environ.get('FLASK_ENV', '')
+    if flask_env == 'dev':
+        app.run(host='0.0.0.0', port=port, debug=True)
+    else:
+        app.run(host='0.0.0.0', port=port)
