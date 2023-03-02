caa766460d1f277a91196fd37650dc29cc84abd6
ceshmina <shu@ceshmina.com>
2023-03-02 22:18:15 +0900

    Firestoreから日記を取得

--- python/main.py
+++ python/main.py
@@ -1,5 +1,5 @@
-from google.cloud import firestore
 from flask import Flask
+from google.cloud import firestore
 import os
 
 
@@ -13,7 +13,12 @@ def index():
 
 @app.route('/diary')
 def diary():
-    return 'まだ日記はありません'
+    fs_client = firestore.Client()
+    entries = fs_client.collection('contents').document('diary') \
+        .collection('months').document('202301') \
+        .collection('entries').order_by('date', direction='DESCENDING').stream()
+
+    return [entry.to_dict() for entry in entries]
 
 
 if __name__ == '__main__':
