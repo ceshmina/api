88bc41e9206dbbe6a52b69d363c9ab939cd40229
ceshmina <shu@ceshmina.com>
2023-03-04 17:53:16 +0900

    指定した月の日記を取得するように変更

--- python/app/diary.py
+++ python/app/diary.py
@@ -1,11 +1,19 @@
+from datetime import date
 from google.cloud import firestore
 from typing import Any, Dict, List
 
 
-def diary() -> List[Dict[str, Any]]:
+def diary(month: str) -> List[Dict[str, Any]]:
+    try:
+        query_date = date(int(month[:4]), int(month[4:6]), 1)
+    except Exception:
+        raise Exception('Invalid requested month: month must be specified as yyyymm')
+
+    query_month = query_date.strftime('%Y%m')
+
     fs_client = firestore.Client()
     entries = fs_client.collection('contents').document('diary') \
-        .collection('months').document('202301') \
+        .collection('months').document(query_month) \
         .collection('entries').order_by('date', direction='DESCENDING').stream()
 
     return [entry.to_dict() for entry in entries]
--- python/web/routes.py
+++ python/web/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 
 from app.index import index
 from app.diary import diary
+from web.response import build_response
 
 
 routes = Blueprint('routes', __name__)
@@ -9,9 +10,9 @@ routes = Blueprint('routes', __name__)
 
 @routes.route('/')
 def route_index():
-    return index()
+    return build_response(index)
 
 
-@routes.route('/diary')
-def route_diary():
-    return diary()
+@routes.route('/diary/<month>')
+def route_diary(month: str):
+    return build_response(diary, month=month)
