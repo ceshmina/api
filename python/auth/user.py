b39ae5feb0d1da8fb9ef50cb81ba9b563d34eeb5
ceshmina <shu@ceshmina.com>
2023-03-04 22:02:30 +0900

    パスワードをハッシュ化

--- python/auth/user.py
+++ python/auth/user.py
@@ -1,5 +1,6 @@
 from flask_login import UserMixin
 from google.cloud import firestore
+import hashlib
 from typing import Optional
 
 
@@ -12,8 +13,10 @@ def get_user(user_id: str, password: str) -> Optional[User]:
     fs_client = firestore.Client()
     users = fs_client.collection('users').where('user_id', '==', user_id).stream()
 
+    password_hash = hashlib.sha256(bytes(password, 'utf-8')).hexdigest()
+
     for user in users:
-        if user_id == user.get('user_id') and password == user.get('password'):
+        if user_id == user.get('user_id') and password_hash == user.get('password'):
             return User(user_id)
     
     return None
